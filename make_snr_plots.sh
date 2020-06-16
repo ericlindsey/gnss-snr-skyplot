c888226062d6f5d4776550c9081c3404bfe88ce8
Eric Lindsey <ericl256@gmail.com>
2020-06-16 20:56:07 +0800

    timeseries plot with skymap

--- make_snr_plots.sh
+++ make_snr_plots.sh
@@ -8,6 +8,8 @@ if [[ $# -lt 1 ]]; then
   exit 1
 fi
 
+gmt gmtset PS_MEDIA A0
+
 # azimuth/elevation resolution for plots
 azincr=10
 elincr=5
@@ -45,48 +47,49 @@ N	127.5
 EOF
 fi
 
-# loop over files - extract SNR by azimuth and elevation
 mkdir -p snr
 mkdir -p sp3
-# loop over all input rinex files.
+mkdir -p grd
+mkdir -p dat
+mkdir -p ts_grd
+rm -f ts_grd/*
+mkdir -p plots
+# loop over all input rinex files, extract SNR by azimuth and elevation
 for file in $@
 do
+  echo working on $file
   base=$(basename $file)
-  echo working on $base
   site=${base:0:4}
   doy=${base:4:3}
   yr=${base:9:2}
-  stem=$(basename $base .${yr}o)
+  snrfile=snr/$base.snr$snrtype 
 
   # Check if the corresponding SNR file exists already, if not, create it.
-  if [[ ! -f snr/$stem.snr$snrtype ]]; then
+  if [[ ! -f $snrfile ]]; then
 
     # get week and day of week from gamit 'doy' program
     # this requires gamit_globk to be installed (also required for getting the orbits)
     wk=`doy $yr $doy |awk 'NR==2{print $3}'`
     dow=`doy $yr $doy |awk 'NR==2{print $7}' | awk -F, '{print $1}'`
     sp3="sp3/igs${wk}${dow}.sp3"
-    echo $sp3
     if [[ ! -f $sp3 ]]; then
         echo "expected orbit file $sp3 not found. Downloading from archive:"
         cd sp3
-        ~/gg/com/sh_get_orbits -yr $yr -doy $doy -makeg no
-        #~/gg/com/sh_get_orbits -orbit igsf -nofit -yr $yr -doy $doy
+        #~/gg/com/sh_get_orbits -yr $yr -doy $doy -makeg no
+        ~/gg/com/sh_get_orbits -orbit igsf -nofit -yr $yr -doy $doy
         cd ..
     fi
     
-    ln -s $file .
-    echo "./gnssSNR/gnssSNR.e $base $stem.snr$snrtype $sp3 $snrtype"
-    ./gnssSNR/gnssSNR.e $base $stem.snr$snrtype $sp3 $snrtype
-
-    mv $stem.snr$snrtype snr/
+    echo "./gnssSNR/gnssSNR.e $file $snrfile $sp3 $snrtype"
+    ./gnssSNR/gnssSNR.e $file $snrfile $sp3 $snrtype
 
   fi
+
   # grid each day, for timeseries
   if [[ $obs == "S1" ]]; then
-    awk '{print $3,$2,$7}' snr/$stem.snr$snrtype > temp.dat 
+    awk '{print $3,$2,$7}' $snrfile > temp.dat 
   else
-    awk '{print $3,$2,$8}' snr/$stem.snr$snrtype > temp.dat 
+    awk '{print $3,$2,$8}' $snrfile > temp.dat 
   fi
   gmt blockmean temp.dat -R0/360/0/85 -I$ts_azincr/$ts_elincr |gmt xyz2grd -R0/360/0/85 -I$ts_azincr/$ts_elincr -r -Gts_grd/${site}_${yr}_${doy}_S1_$ts_azincr-$ts_elincr.grd
 done
@@ -98,21 +101,22 @@ gmt makecpt -Cpolar -T-10/10/1 -D -Z >polar10.cpt
 # get list of unique 4-character site names
 ls snr/* |cut -f2 -d/ | awk '{printf("%.4s\n",$1)}' |uniq > siteslist.txt
 
-# # convert SNR output to GMT grid files
-# mkdir -p grd
-# mkdir -p ts_grd
-# for site in `cat siteslist.txt`
-# do
-#   echo gridding SNR values: $site
-#   #grid all data for each day for averaged sky plots
-#   cat snr/$site????.snr$snrtype > snr/$site.snr$snrtype
-#   if [[ $obs == "S1" ]]; then
-#     awk '{print $3,$2,$7}' snr/$site.snr$snrtype > temp.dat 
-#   else 
-#     awk '{print $3,$2,$8}' snr/$site.snr$snrtype > temp.dat 
-#   fi
-#   gmt blockmean temp.dat -R0/360/0/85 -I$azincr/$elincr |gmt xyz2grd -R0/360/0/85 -I$azincr/$elincr -Ggrd/${site}_S1_$azincr-$elincr.grd
-# done
+# average SNR output to single GMT grid files for each site
+for site in `cat siteslist.txt`
+do
+  echo gridding last 30 SNR values: $site
+  rm -f snr/$site.snr$snrtype
+  for recent in `ls -tr snr/$site????.??o.snr$snrtype |tail -n30`
+  do
+     cat $recent >> snr/$site.snr$snrtype
+  done
+  if [[ $obs == "S1" ]]; then
+    awk '{print $3,$2,$7}' snr/$site.snr$snrtype > temp.dat 
+  else 
+    awk '{print $3,$2,$8}' snr/$site.snr$snrtype > temp.dat 
+  fi
+  gmt blockmean temp.dat -R0/360/0/85 -I$azincr/$elincr |gmt xyz2grd -R0/360/0/85 -I$azincr/$elincr -Ggrd/${site}_S1_$azincr-$elincr.grd
+done
 # 
 #  # average combined grid files by elevation
 #  rm -rf dat
@@ -126,7 +130,6 @@ ls snr/* |cut -f2 -d/ | awk '{printf("%.4s\n",$1)}' |uniq > siteslist.txt
 #  cat dat/????_${obs}_elevmean.dat | awk '{print 0,$1,$2}' | gmt blockmean -R0/360/0/85 -I800/$elincr |awk '{print $2,$3}' > dat/all_${obs}_elevmean.dat
 #  
 #  #make elevation-angle plots
-#  mkdir -p plots
 #  gmt gmtset MAP_TICK_LENGTH -5p
 #  gmt gmtset MAP_LABEL_OFFSET 15p
 #  gmt gmtset FONT_TITLE 16p
@@ -164,17 +167,17 @@ ls snr/* |cut -f2 -d/ | awk '{printf("%.4s\n",$1)}' |uniq > siteslist.txt
 #  for site in `cat siteslist.txt`
 #  do
 #    echo making skyplots for $site
-#    gmt grd2xyz grd/${site}_${obs}_${azincr}-${elincr}_diff.grd -s |awk -v az=$azincr -v el=$elincr '{printf("%f %f %f %f %fd %fd\n",$1,-$2,$3,0,el,az*cos($2*3.14159/180))}' |gmt psxy -SJ -Cpolar10.cpt -R0/360/-90/0 -JS0/-90/5i -Ba90f30g30":.$site ${obs}:" -P -K >  plots/${site}_skyplot_diff.ps
-#    gmt psscale  -D5.7i/6.5/10/0.6 -Cpolar10.cpt -E -B5::/:"delta SNR": -O -K >> plots/${site}_skyplot_diff.ps
+#    gmt grd2xyz grd/${site}_${obs}_${azincr}-${elincr}_diff.grd -s |awk -v az=$azincr -v el=$elincr '{printf("%f %f %f %f %fd %fd\n",$1,-$2,$3,0,el,az*cos($2*3.14159/180))}' |gmt psxy -SJ -Cpolar10.cpt -R0/360/-90/0 -JS0/-90/5i -Ba90f30g30":.$site ${obs}:" -P -K >  plots/${site}_${obs}_skyplot_diff.ps
+#    gmt psscale  -D5.7i/6.5/10/0.6 -Cpolar10.cpt -E -B5::/:"delta SNR": -O -K >> plots/${site}_${obs}_skyplot_diff.ps
 #    
-#    gmt psxy -J -R -O -T >> plots/${site}_skyplot_diff.ps
-#    gmt psconvert -A1p -Tg plots/${site}_skyplot_diff.ps
+#    gmt psxy -J -R -O -T >> plots/${site}_${obs}_skyplot_diff.ps
+#    gmt psconvert -A1p -Tg plots/${site}_${obs}_skyplot_diff.ps
 #  
-#    gmt grd2xyz grd/${site}_${obs}_${azincr}-${elincr}.grd -s |awk -v az=$azincr -v el=$elincr '{printf("%f %f %f %f %fd %fd\n",$1,-$2,$3,0,el,az*cos($2*3.14159/180))}' |gmt psxy -SJ -Cmagma_$obs.cpt -R0/360/-90/0 -JS0/-90/5i -Ba90f30g30":.$site ${obs}:" -P -K >  plots/${site}_skyplot.ps
-#    gmt psscale  -D5.7i/6.5/10/0.6 -Cmagma_$obs.cpt -E -B25::/:"GPS SNR": -O -K >> plots/${site}_skyplot.ps
+#    gmt grd2xyz grd/${site}_${obs}_${azincr}-${elincr}.grd -s |awk -v az=$azincr -v el=$elincr '{printf("%f %f %f %f %fd %fd\n",$1,-$2,$3,0,el,az*cos($2*3.14159/180))}' |gmt psxy -SJ -Cmagma_$obs.cpt -R0/360/-90/0 -JS0/-90/5i -Ba90f30g30":.$site ${obs}:" -P -K >  plots/${site}_${obs}_skyplot.ps
+#    gmt psscale  -D5.7i/6.5/10/0.6 -Cmagma_$obs.cpt -E -B25::/:"GPS SNR": -O -K >> plots/${site}_${obs}_skyplot.ps
 #    
-#    gmt psxy -J -R -O -T >> plots/${site}_skyplot.ps
-#    gmt psconvert -A1p -Tg plots/${site}_skyplot.ps
+#    gmt psxy -J -R -O -T >> plots/${site}_${obs}_skyplot.ps
+#    gmt psconvert -A1p -Tg plots/${site}_${obs}_skyplot.ps
 #  done
 
 # make timeseries and plots
@@ -196,6 +199,7 @@ do
 
     #simplistic method: convert to an xyz file, then move each line to the correct timeseries file
     NR=0
+    echo $file
     gmt grd2xyz $file > temp.dat
     while read line
     do
@@ -244,8 +248,9 @@ do
   done
   # create skymap index with time-averaged values on the side
   gmt grd2xyz grd/${site}_${obs}_${azincr}-${elincr}.grd -s |awk -v az=$azincr -v el=$elincr '{printf("%f %f %f %f %fd %fd\n",$1,-$2,$3,0,el,az*cos($2*3.14159/180))}' |gmt psxy -SJ -Cmagma_$obs.cpt -R0/360/-90/0 -JS0/-90/3i -Ba90f30g30 -O -K -X7i >>  plots/${site}_${obs}_timeseries.ps
-  gmt psxy dat/ptsfile.dat -Sc0.4c -W0.5p,black -G+z -Ccolors12_custom.cpt -J -R -O -K >> plots/${site}_${obs}_timeseries.ps
-  gmt psscale  -Dx3.5i/0i+w3i/0.3i -Cmagma_$obs.cpt -E -B10::/:"$obs SNR (dB)": -O -K >> plots/${site}_${obs}_timeseries.ps
+  gmt psxy dat/ptsfile.dat -Sp0.4c -Ccolors12_custom.cpt -J -R -O -K >> plots/${site}_${obs}_timeseries.ps
+  gmt psxy dat/ptsfile.dat -Sc0.4c -W0.5p,black -J -R -O -K >> plots/${site}_${obs}_timeseries.ps
+  gmt psscale  -Dx3.5i/0i+w3i/0.3i+e -Cmagma_$obs.cpt -B10::/:"$obs SNR (dB)": -O -K >> plots/${site}_${obs}_timeseries.ps
 
   #finalize timeseries plot
   gmt psxy -J -R -O -T >> plots/${site}_${obs}_timeseries.ps
@@ -253,5 +258,5 @@ do
 done
 
 #rm -f plots/*.ps
-rm -f temp.dat
+#rm -f temp.dat
 
