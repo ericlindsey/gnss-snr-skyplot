0a71ad57946e1364da626d49cc1ad135477a9671
Eric Lindsey <ericl256@gmail.com>
2020-10-12 16:52:34 -0600

    fix bug in test script

--- test_snr_plots.sh
+++ test_snr_plots.sh
@@ -3,7 +3,8 @@
 #script to test that you have installed everything correctly
 
 # test rinex file has a non-standard name, link it as a standard name temporarily
-ln -s gnssSNR/at013620.18o.test at013620.18o
+mkdir -p rinex
+ln -s gnssSNR/at013620.18o.test rinex/at013620.18o
 # run the gnssSNR test data
 ./grid_snr_data rinex/at013620.18o
 # remove link
