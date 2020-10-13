8788cfca503adb47eeb7b084bb821100ee16d18d
Eric Lindsey <ericl256@gmail.com>
2020-10-12 18:37:19 -0600

    update to run only skyplot creation

--- test_snr_plots.sh
+++ test_snr_plots.sh
@@ -2,14 +2,20 @@
 
 #script to test that you have installed everything correctly
 
-# test rinex file has a non-standard name, link it as a standard name temporarily
+# the standard test rinex file has a non-standard name, so first we link it as a standard name temporarily
 mkdir -p rinex
-ln -s gnssSNR/at013620.18o.test rinex/at013620.18o
-# run the gnssSNR test data
-./grid_snr_data rinex/at013620.18o
+ln -s ../gnssSNR/at013620.18o.test rinex/at013620.18o
+
+# extract the SNR data using the gnssSNR library
+./extract_snr_data.sh rinex/at013620.18o
+
 # remove link
-rm -f at013620.18o
+rm -f rinex/at013620.18o
+
 # make plots
-./make_skymaps.sh
-./make_timeseries_plots.sh
+./make_skymaps.sh at01
+
+# timeseries - note, this doesn't work with only one file...
+# ./grid_for_timeseries.sh snr/at01*
+# ./make_timeseries_plots.sh at01
 
